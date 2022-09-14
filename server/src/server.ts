8b053cdc7cfeb53493783b1af7e733c053bf199a
VitorMendesco <vitor.mcosta@sptech.school>
2022-09-14 10:37:54 -0300

    build: server-side initial config w/ express and ts

---
+++ server/src/server.ts
@@ -0,0 +1,14 @@
+import express from "express";
+
+const app = express();
+
+app.get("/ads", (req, res) => {
+  console.log("access");
+  return res.json([
+    {id: 1, name: "ale1"},
+    {id: 2, name: "ale2"},
+    {id: 3, name: "ale3"},
+  ]);
+});
+
+app.listen(3333);
