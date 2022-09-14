50b22cb624b4b2b39b1128b3d5ce48df41c5c634
VitorMendesco <vitor.mcosta@sptech.school>
2022-09-14 15:59:40 -0300

    build: tailwind config aded

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,9 +1,16 @@
+import './styles/main.css';
+
+import logoImg from './assets/logo-esports.svg';
+
 function App() {
+
   return (
-    <div className="App">
-      <h1>Hello man</h1>
+    <div className='max-w-[1344px] mx-auto flex flex-col items-center my-20'>
+      <img src={logoImg} alt="eSports Logo" />
+
+      <h1 className='text-6xl text-white font-black mt-20'>Your <span className='text-transparent bg-duoGradient bg-clip-text'>duo</span> is here!</h1>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
