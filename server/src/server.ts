bd759915d056ad2ff57ecf4f89befec82598934c
VitorMendesco <vitor.mcosta@sptech.school>
2022-09-14 10:50:48 -0300

    build: initial server config finished with automation of @types/express

--- server/src/server.ts
+++ server/src/server.ts
@@ -4,11 +4,7 @@ const app = express();
 
 app.get("/ads", (req, res) => {
   console.log("access");
-  return res.json([
-    {id: 1, name: "ale1"},
-    {id: 2, name: "ale2"},
-    {id: 3, name: "ale3"},
-  ]);
+  return res.json([]);
 });
 
 app.listen(3333);
